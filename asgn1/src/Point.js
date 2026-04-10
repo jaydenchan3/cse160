350fda717d5feaa4b72951aa89ebb4134da9b4ab
jaydenchan3 <123054797+jaydenchan3@users.noreply.github.com>
2026-04-10 00:39:33 -0700

    asgn1

---
+++ asgn1/src/Point.js
@@ -0,0 +1,20 @@
+class Point {
+  constructor() {
+    this.position = [0, 0];
+    this.color    = [1.0, 1.0, 1.0, 1.0];
+    this.size     = 10;
+    this.segments = 0;
+  }
+
+  render() {
+    gl.uniform4f(u_FragColor, this.color[0], this.color[1], this.color[2], this.color[3]);
+    gl.uniform1f(u_Size, this.size);
+
+    var buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.position), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
+}
